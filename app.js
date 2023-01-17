8930cfbb3d8f50daa6bdc5b7a41091d55b962c3d
Lars <l.cornelissen5@student.han.nl>
2023-01-17 12:15:30 +0100

    readme parser fix

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 
 window.addEventListener("load", function () {
-const input = document.getElementById('input-field');
-const output = document.getElementById('output-field');
-const converter = new showdown.Converter();
+    const input = document.getElementById('input-field');
+    const output = document.getElementById('output-field');
+    const converter = new showdown.Converter();
 
-const parseRepoButton = document.getElementById('repo-button');
-const parseRepoText = document.getElementById('repo-text');
+    const parseRepoButton = document.getElementById('repo-button');
+    const parseRepoText = document.getElementById('repo-text');
 
-input.addEventListener('input', updateMarkown);
-output.addEventListener('input', updateHTML);
-parseRepoButton.addEventListener('click', parseRepo);
+    input.addEventListener('input', updateMarkown);
+    output.addEventListener('input', updateHTML);
+    parseRepoButton.addEventListener('click', parseRepo);
 
-function updateMarkown() {
-    input.style.height = 'auto';
-    input.style.height = input.scrollHeight + 10 + 'px';
-    output.innerHTML = converter.makeHtml(input.value);
-}
+    function updateMarkown() {
+        input.style.height = 'auto';
+        input.style.height = input.scrollHeight + 10 + 'px';
+        output.innerHTML = converter.makeHtml(input.value);
+    }
 
-function updateHTML() {
-    input.value = converter.makeMd(output.innerHTML);
-}
+    function updateHTML() {
+        input.value = converter.makeMd(output.innerHTML);
+    }
 
-async function getRepoData(username, repoName) {
-    const apiUrl = `https://api.github.com/repos/${username}/${repoName}`;
-    return await fetch(apiUrl);
-}
-async function getLanguages(username, repoName) {
-    const apiUrl = `https://api.github.com/repos/${username}/${repoName}/languages`;
-    return await fetch(apiUrl);
-}
+    async function getRepoData(username, repoName) {
+        const apiUrl = `https://api.github.com/repos/${username}/${repoName}`;
+        return await fetch(apiUrl);
+    }
+    async function getLanguages(username, repoName) {
+        const apiUrl = `https://api.github.com/repos/${username}/${repoName}/languages`;
+        return await fetch(apiUrl);
+    }
 
-async function parseRepo(event) {
-    event.preventDefault();
-    const repoUrl = parseRepoText.value;
-    const [, , , username, repoName] = repoUrl.split('/');
-    try {
-        const data = await (await getRepoData(username, repoName)).json();
-        const languages = await (await getLanguages(username, repoName)).json();
-        input.value = `
+    async function parseRepo(event) {
+        event.preventDefault();
+        const repoUrl = parseRepoText.value;
+        const [, , , username, repoName] = repoUrl.split('/');
+        try {
+            const data = await (await getRepoData(username, repoName)).json();
+            const languages = await (await getLanguages(username, repoName)).json();
+            input.value = `
 # ${data.name}
 
 <p align="center">
@@ -105,12 +105,12 @@ For support, open a pull request or open an issue [here](https://github.com/${us
 
 This project is licensed under the <a href="${data.license.url}}">${data.license.name}</a> license.
         `;
-        updateMarkown();
-    } catch (error) {
-        console.error(error);
+            updateMarkown();
+        } catch (error) {
+            console.error(error);
+        }
     }
-}
 
 
-updateMarkown();
+    updateMarkown();
 });
